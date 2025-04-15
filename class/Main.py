843bdbd4e976f8b19b0a8c81d730ce01264678d0
fannyg08 <153272764+fannyg08@users.noreply.github.com>
2025-04-15 22:05:54 +0200

    Update Main.py

--- class/Main.py
+++ class/Main.py
@@ -1,8 +1,9 @@
 from ClassMarket import Underlying, MarketData
-from ClassProduit import AutocallNote, CapitalProtectedNote
+from ClassProduit import AutocallNote, CapitalProtectedNote, CapitalProtectedNoteWithBarrier
 from ClassPricing import PricingEngine
 
-# Créer les données de marché
+
+
 market_data = MarketData(
     spot_price=100.0,
     volatility=0.20,
@@ -10,9 +11,24 @@ market_data = MarketData(
     dividend_yield=0.01
 )
 
+underlying = Underlying("EURO STOXX 50", market_data)
+
+# Créer les différents produits
+barrier_note = CapitalProtectedNoteWithBarrier(
+    underlying=underlying,
+    maturity=5.0,
+    nominal=1000.0,
+    strike=100.0,
+    barrier=130.0,
+    participation_rate=1.0,
+    capital_protection=1.0,
+    rebate=0.05
+)
 # Créer le sous-jacent
 underlying = Underlying("EURO STOXX 50", market_data)
 
+"""""
+# A l'heure actuelle il faut réadapter le code de pricing engine
 # Créer la note à capital protégé
 capital_protected_note = CapitalProtectedNote(
     underlying=underlying,
@@ -25,13 +41,9 @@ capital_protected_note = CapitalProtectedNote(
 
 # Créer le moteur de pricing
 pricing_engine = PricingEngine(capital_protected_note, nb_simulations=10000)
-
+"""
 # Calculer le prix
-price = pricing_engine.calculate_price()
-print(f"Prix de la note à capital protégé: {price:.2f}")
+barrier_engine = PricingEngine(barrier_note, nb_simulations=10000)
+price = barrier_engine.calculate_price()
+print(f"Prix de la note avec barrière: {price:.2f}")
 
-# Calculer les Greeks
-greeks = pricing_engine.calculate_greeks()
-print(f"Delta: {greeks['delta']:.4f}")
-print(f"Vega: {greeks['vega']:.4f}")
-print(f"Theta: {greeks['theta']:.4f}")
